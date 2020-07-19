09a67dd28bf9d7e6dc39a8be251b0cb918a28756
desoares1975 <fabio.desoares@gmail.com>
2020-07-19 13:00:00 -0300

    add air bnb

--- sh.sh
+++ sh.sh
@@ -3,6 +3,6 @@
 mkdir test
 
 cp -R files/. ../../
-npm i -g eslint husky lint-staged airbnb-base
+npm i -g airbnb eslint eslint-config-airbnb-base eslint-config-prettier eslint-plugin-import eslint-plugin-prettier husky lint-staged prettier
 
 echo All files copied and all packacages installed
