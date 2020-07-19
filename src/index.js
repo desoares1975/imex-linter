d33077175375eb16c2b0a01569ff3d0532381a97
desoares1975 <fabio.desoares@gmail.com>
2020-07-18 23:59:30 -0300

    other test

--- src/index.js
+++ src/index.js
@@ -2,12 +2,11 @@ const jsonfile = require('jsonfile')
 
 module.exports = (() => {
   try {
-
-    const packaged = jsonfile.readFileSync('package.json')
+    const packaged = jsonfile.readFileSync('./package.json')
 
     packaged.scripts = packaged.scripts || {}
     packaged.scripts['do:it'] = "./node_modules/immex-linter/sh.sh doit"
-    jsonfile.writeFileSync('package.json', packaged, { spaces: 2 })
+    jsonfile.writeFileSync('./package.json', packaged, { spaces: 2 })
   } catch (e) {
     console.log(e)
   }
