9c8a51af117b58e52dd2d7d7289d70e38254075c
desoares1975 <fabio.desoares@gmail.com>
2020-07-18 23:35:49 -0300

    ok now

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const jsonfile = require('jsonfile')
 
 module.exports = (() => {
-  const packaged = jsonfile.readFileSync('package.json')
+  const packaged = jsonfile.readFileSync('../package.json')
+
   packaged.scripts = packaged.scripts || {}
   packaged.scripts['do:it'] = "./node_modules/immex-linter/sh.sh doit"
-  jsonfile.writeFileSync('package.json', packaged, { spaces: 2 })
+  jsonfile.writeFileSync('../package.json', packaged, { spaces: 2 })
 })()
