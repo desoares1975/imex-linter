ca7fcfaadb6bb0e520dcc0d03d2b19d70b59960c
desoares1975 <fabio.desoares@gmail.com>
2020-07-19 22:07:55 -0300

    reshaping

--- sh.sh
+++ sh.sh
@@ -4,6 +4,6 @@ mkdir test
 
 cp -R files/. ../../
 cd ../../ && npm i -D airbnb eslint eslint-config-airbnb-base eslint-config-prettier eslint-plugin-import eslint-plugin-prettier husky lint-staged prettier
-npm remove --save imex-linter
+npm remove imex-linter
 
 echo All files copied and all packacages installed
